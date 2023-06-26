47351e28874a4045a8b1dc155e003c34d5d166dd
dinhmn <mai.ngoc.dinh@systemi.co.jp>
2023-06-26 13:39:39 +0700

    feature/config: Multiple language.

---
+++ multipleLanguageWebApplication/src/main/java/com/webapplication/multipleLanguageWebApplication/MultipleLanguageWebApplication.java
@@ -0,0 +1,13 @@
+package com.webapplication.multipleLanguageWebApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultipleLanguageWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MultipleLanguageWebApplication.class, args);
+	}
+
+}
