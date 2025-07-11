f14507225eb00fb1a1c22f90762a19b9f7f0e34c
mai.ngoc.dinh <ngocdinh2k1@gmail.com>
2025-07-11 14:41:50 +0700

    Initial websocket

---
+++ Redis-Socket-EC2/src/main/java/com/socket_ec2/Redis_Socket_EC2/RedisSocketEc2Application.java
@@ -0,0 +1,13 @@
+package com.socket_ec2.Redis_Socket_EC2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedisSocketEc2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RedisSocketEc2Application.class, args);
+	}
+
+}
---
+++ Redis-Socket-EC2/src/test/java/com/socket_ec2/Redis_Socket_EC2/RedisSocketEc2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.socket_ec2.Redis_Socket_EC2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RedisSocketEc2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
